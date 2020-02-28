a1cdb5a8c4a14aa6f38376d61a566f60954d1954
calltovishal <calltovishal@gmail.com>
2020-02-28 11:24:11 +0530

    routing handle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = ({ addedItems }) => {
                         <Link to="/" className="brand-logo"><i className="fa fa-home" aria-hidden="true"></i></Link>
                     </div>
                     <div className="col-sm-9">
-                        <span className="float-right headerRight"><i className="fa fa-search" aria-hidden="true"></i><Link to="/cart"><i className="fa fa-shopping-cart"></i><sup style={{ color: 'white' }}>{totalQty}</sup></Link></span>                        
+                        <span className="float-right headerRight"><Link to="/cart"><i className="fa fa-shopping-cart"></i><sup style={{ color: 'white' }}>{totalQty}</sup></Link></span>                        
                     </div>
                 </div>
             </div>
